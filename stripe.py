20c3de8c690c74164ba4f6d92a30123e1fae99ab
Amarnath C <amarnathcharichilil@gmail.com>
2022-06-30 07:47:39 +0530

    Update stripe.py

--- stripe.py
+++ stripe.py
@@ -28,36 +28,37 @@ browser = webdriver.Chrome(options=chrome_options)
 async def pay(cc, exp_mo, exp_yr, cvc):
     browser.delete_all_cookies()
     browser.get("https://martialartsolympia.com/virtual-classes-signup")
-    wait = WebDriverWait(browser, 10)
-    wait.until(EC.element_to_be_clickable(
+    wait = WebDriverWait(browser, 15)
+    try:
+     wait.until(EC.element_to_be_clickable(
         (By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div[2]/div[1]/input[1]")))
-    browser.find_element(
+     browser.find_element(
         By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div[2]/div[1]/input[1]").send_keys(gen_random_name())
-    browser.find_element(
+     browser.find_element(
         By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div[2]/div[1]/input[2]").send_keys(gen_random_name() + "@gmail.com")
-    password = gen_random_name()
-    browser.find_element(
+     password = gen_random_name()
+     browser.find_element(
         By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div[2]/div[1]/div/div[1]/input[1]").send_keys(password)
-    browser.find_element(
+     browser.find_element(
         By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div[2]/div[1]/div/div[1]/input[2]").send_keys(password)
-    browser.find_element(
+     browser.find_element(
         By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div[2]/div[2]/input").send_keys(gen_random_us_phone())
-    browser.find_element(
+     browser.find_element(
         By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div[4]/section/div[2]/label[2]").click()
-    browser.switch_to.frame(browser.find_element(
+     browser.switch_to.frame(browser.find_element(
         By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div[4]/section/div[2]/label[2]/div/label/div/div/iframe"))
-    browser.find_element(By.NAME, "cardnumber").send_keys(cc)
-    browser.find_element(By.NAME, "exp-date").send_keys(exp_mo + "/" + exp_yr)
-    browser.find_element(By.NAME, "cvc").send_keys(cvc)
-    browser.switch_to.default_content()
-    wait.until(EC.element_to_be_clickable((
+     browser.find_element(By.NAME, "cardnumber").send_keys(cc)
+     browser.find_element(By.NAME, "exp-date").send_keys(exp_mo + "/" + exp_yr)
+     browser.find_element(By.NAME, "cvc").send_keys(cvc)
+     browser.switch_to.default_content()
+     wait.until(EC.element_to_be_clickable((
         By.NAME, "field0")
     ))
-    browser.find_element(
+     browser.find_element(
         By.NAME, "field0").send_keys(gen_random_name())
-    browser.find_element(
+     browser.find_element(
         By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[5]/div/div/div[2]/a[2]").click()
-    try:
+     try:
         wait.until(EC.element_to_be_clickable((
             By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[5]/div/div/div[2]/div")
         ))
@@ -68,10 +69,11 @@ async def pay(cc, exp_mo, exp_yr, cvc):
         if "." in box.text[:len(box.text) - 1]:
             return "declined", box.text[:box.text.find(".")], box.text[box.text.find(".") + 1:].strip(), browser
         return "declined", box.text, "N/A", browser
-    except NoSuchElementException:
+     except NoSuchElementException:
         browser.set_window_size(1920, 1080)
         return "error", "N/A", "N/A", browser
-
+    except:
+        return "crashed", "N/Q", "N/A", browser
 
 def gen_random_us_phone():
     '''
